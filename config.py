58c85a4375158b5310ae6678e7faa89857d64645
Kulik1987 <kulik7771@ya.ru>
2024-08-03 12:38:58 +0300

    create bot

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', '7277068779:AAFlZluICQO6HXBenkU8gc_UxtpCmJdG_u4')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID', '-1002202830500')
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 API_ID = os.getenv('API_ID', '22444023')
 API_HASH = os.getenv('API_HASH', '4c086295041aea733c379dfe5dc17f02')
